72447f61fe0ea5d54648a02adac69d66d866f17b
Shahin M Hashim <shahinmhashim@gmail.com>
2023-09-17 11:58:53 +0530

    completed section-1 of the page

---
+++ interaction.js
@@ -0,0 +1,21 @@
+window.onload =function()
+{
+ console.log(`The Page Has Loaded Successfully${"\n"}Welcome To My Portfolio`);
+}
+
+
+// website-tagline-txt
+var typed1 = new Typed('#slog-txt1', {
+    strings: ['Websites, Images And Videos'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    showCursor: false,
+    loop: false
+});
+
+var typed2 = new Typed('#slog-txt2', {
+    strings: ['That Just Feels RIGHT'],
+    typeSpeed: 150,
+    backSpeed: 110,
+    loop: true
+});
