0d67ac49d6f137165876d79c04e37986d78dd664
Shahin M Hashim <shahinmhashim@gmail.com>
2023-09-24 23:22:28 +0530

    modified intro-description and each section with padding,background etc

--- interaction.js
+++ interaction.js
@@ -3,7 +3,6 @@ window.onload =function()
  console.log(`The Page Has Loaded Successfully${"\n"}Welcome To My Portfolio`);
 }
 
-
 //  website-tagline-txt
     var typed1 = new Typed('#slog-txt1', {
         strings: ['Websites, Images And Videos'],
@@ -19,6 +18,48 @@ window.onload =function()
         backSpeed: 110,
         loop: true
     });
+
+// section 1 intro description
+
+
+let words = document.querySelectorAll(".word");
+words.forEach((word)=>{
+    let letters = word.textContent.split(""); word.textContent="";
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () => {
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        },i * 80);
+    });
+
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        },340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText,5000);
+
 
 //section 2 about 
     ScrollReveal().reveal('.scroll-reveal', {
